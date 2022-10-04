e7c4d06146f5c58667065e1c3561f45eb3e1fbe7
Rodrigo Etchegaray <rorroeche@gmail.com>
2022-10-03 21:24:01 -0300

    Example working

--- app/app.js
+++ app/app.js
@@ -8,13 +8,25 @@ const id = uuid.v4();
 const PORT = 3000;
 const TIMEOUT = 5 * 1000;
 
-sdc = new SDC({host: 'localhost', port: 8125});
+// sdc = new SDC({host: 'localhost', port: 8125});
+var lynx = require('lynx');
+var metrics = new lynx('graphite', 8125);
 
 app.get('/', (req, res) => {
-    const begin = new Date();
-    sdc.timing('helloserver.hello-world.request-time', begin);
-    sdc.increment('helloserver.hello-world.request-count');
-    sdc.gauge('some.gauge', 10); // Set gauge to 10
+    // const begin = new Date();
+    // sdc.timing('helloserver.hello-world.request-time', begin);
+    // sdc.increment('helloserver.hello-world.request-count');
+    // sdc.gauge('some.gauge', 10); // Set gauge to 10
+
+    console.log('Creo una metrica');
+
+    metrics.increment('node_test.int');
+    metrics.decrement('node_test.int');
+    metrics.timing('node_test.some_service.task.time', 500); // time in ms
+    metrics.gauge('fromcode.one', 100);
+    metrics.set('set.one', 10);
+
+
     res.status(200).send(`[${id}] ping\n`);
 });
 
